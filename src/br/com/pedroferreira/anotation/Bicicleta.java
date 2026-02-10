74dedf8d1a789f881ece679741b45e5a205655b0
unknown <pedrohenriquef278@gmail.com>
2026-02-10 17:31:08 -0300

    init commit

---
+++ src/br/com/pedroferreira/anotation/Bicicleta.java
@@ -0,0 +1,11 @@
+package br.com.pedroferreira.anotation;
+
+import java.lang.annotation.Target;
+
+/**
+ * @author Pedro Ferreira
+ */
+@ITabelaAnotation("Bicicletas")
+public class Bicicleta {
+  Double valor;
+}
---
+++ src/br/com/pedroferreira/anotation/Caminhao.java
@@ -0,0 +1,9 @@
+package br.com.pedroferreira.anotation;
+
+/**
+ * @author Pedro Ferreira
+ */
+@ITabelaAnotation("Caminhões")
+public class Caminhao {
+  String Marca;
+}
